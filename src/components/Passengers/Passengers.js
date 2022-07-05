e97bb50a530f091633a644ef63a4a91e7d6bbe6b
Lawrence Dovin <lawrencedovin@gmail.com>
2022-07-05 13:46:39 -0400

    Updated pagination component

---
+++ src/components/Passengers/Passengers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Passengers.css';
+
+const Passengers = () => (
+  <div className="Passengers">
+    Passengers Component
+  </div>
+);
+
+Passengers.propTypes = {};
+
+Passengers.defaultProps = {};
+
+export default Passengers;
