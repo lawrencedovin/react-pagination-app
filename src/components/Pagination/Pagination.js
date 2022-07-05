7c40fa40810e5d5dfd30094500aa1c6a9c74a23b
Lawrence Dovin <lawrencedovin@gmail.com>
2022-07-05 13:24:49 -0400

    Added initial pagination component

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Pagination.css';
+
+const Pagination = () => (
+  <>
+    <div className="Pagination">
+      I am the Paginator. 🤖
+    </div>
+  </>
+);
+
+Pagination.propTypes = {};
+
+Pagination.defaultProps = {};
+
+export default Pagination;
